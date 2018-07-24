9920f951b8b541cb856fe9b032dcd5b339482bbd
Thomas Tilak <thomas.tilak@ign.fr>
2018-07-24 14:53:02 +0200

    fix OGR_G_ImportFromWkb signature changed in https://github.com/OSGeo/gdal/commit/90746662201ee68e467b9d85421ddfe3e169ca08#diff-86e53c6a1e95018240ee21c6b8ce6832

--- ogr.go
+++ ogr.go
@@ -187,10 +187,10 @@ type Geometry struct {
 
 //Create a geometry object from its well known binary representation
 func CreateFromWKB(wkb []uint8, srs SpatialReference, bytes int) (Geometry, error) {
-	cString := (*C.uchar)(unsafe.Pointer(&wkb[0]))
+	pabyData := (unsafe.Pointer(&wkb[0]))
 	var newGeom Geometry
 	return newGeom, C.OGR_G_CreateFromWkb(
-		cString, srs.cval, &newGeom.cval, C.int(bytes),
+		pabyData, srs.cval, &newGeom.cval, C.int(bytes),
 	).Err()
 }
 
@@ -305,8 +305,8 @@ func (geom Geometry) Envelope() Envelope {
 
 // Assign a geometry from well known binary data
 func (geom Geometry) FromWKB(wkb []uint8, bytes int) error {
-	cString := (*C.uchar)(unsafe.Pointer(&wkb[0]))
-	return C.OGR_G_ImportFromWkb(geom.cval, cString, C.int(bytes)).Err()
+	pabyData := (unsafe.Pointer(&wkb[0]))
+	return C.OGR_G_ImportFromWkb(geom.cval, pabyData, C.int(bytes)).Err()
 }
 
 // Convert a geometry to well known binary data
