e0bccc8d2afa78dc593968df5bc085d391459fa9
Thomas Tilak <thomas.tilak@ign.fr>
2018-07-26 08:58:39 +0200

    amend previous commit

--- apps.go
+++ apps.go
@@ -25,6 +25,9 @@ var _ = fmt.Println
 type GDALTranslateOptions struct {
 	cval *C.GDALTranslateOptions
 }
+type GDALWarpAppOptions struct {
+	cval *C.GDALWarpAppOptions
+}
 
 func GDALTranslate(
 	destName string,
@@ -48,6 +51,70 @@ func GDALTranslate(
 		C.CString(destName),
 		srcDS.cval,
 		gdalTranslateOptions.cval,
+		&err,
+	)
+
+	return Dataset{outputDs}
+
+}
+
+// func GDALWarpCreateOutput(
+// 	nSrcCount int,
+// 	srcDS []Dataset,
+// 	pszFilename string,
+// 	pszFormat string,
+// 	papszTO []string,
+// 	ppapszCreateOptions *[]string,
+// 	eDT DataType,
+// 	phTransformArg unsafe.Pointer,
+// 	bSetColorInterpretation bool,
+// 	psOptions GDALWarpAppOptions,
+
+// ) GDALWarpAppOptions {
+
+// 	pahSrcDs := make([]C.GDALDatasetH, len(srcDs)+1)
+// 	for i := 0; i < len(srcDs); i++ {
+// 		pahSrcDs[i] = srcDs[i].cval
+// 	}
+// 	pahSrcDs[len(srcDs)] = (C.GDALDatasetH)(unsafe.Pointer(nil))
+
+// 	hDstDS := C.GDALWarpCreateOutput(
+// 		C.int(nSrcCount),
+// 	)
+// }
+
+func GDALWarp(
+	destName string,
+	dstDs Dataset,
+	srcDs []Dataset,
+	options []string,
+) Dataset {
+
+	var err C.int
+
+	length := len(options)
+	cOptions := make([]*C.char, length+1)
+	for i := 0; i < length; i++ {
+		cOptions[i] = C.CString(options[i])
+		defer C.free(unsafe.Pointer(cOptions[i]))
+	}
+	cOptions[length] = (*C.char)(unsafe.Pointer(nil))
+
+	gdalWarpOptions := GDALWarpAppOptions{C.GDALWarpAppOptionsNew((**C.char)(unsafe.Pointer(&cOptions[0])), nil)}
+
+	pahSrcDs := make([]C.GDALDatasetH, len(srcDs)+1)
+	for i := 0; i < len(srcDs); i++ {
+		pahSrcDs[i] = srcDs[i].cval
+		// defer C.free(unsafe.Pointer(pahSrcDs[i]))
+	}
+	pahSrcDs[len(srcDs)] = (C.GDALDatasetH)(unsafe.Pointer(nil))
+
+	outputDs := C.GDALWarp(
+		C.CString(destName),
+		dstDs.cval,
+		C.int(len(srcDs)),
+		(*C.GDALDatasetH)(unsafe.Pointer(&pahSrcDs[0])),
+		gdalWarpOptions.cval,
 		&err,
 	)
 
